484486c6ecbd0f57eb71d262fc84d271f0c4132f
AMMayberry1 <ammayberry1@gmail.com>
2014-11-17 16:00:31 -0500

    Added LED control project, updated isoch_camera
    Added simple project to use DAC to control NIR LED. Integrated these
    controls into isoch_camera.

--- glasses_firmware/rev3/isoch_camera/main.c
+++ glasses_firmware/rev3/isoch_camera/main.c
@@ -153,3 +153,44 @@ void delay_us(int delayTime)
     asm volatile ("nop\n" "nop\n" "nop\n" "nop\n" "nop\n" "nop\n" "nop\n" "nop\n" "nop\n" "nop\n" "nop\n" "nop\n" "nop\n");
   }
 }
+
+void pulse_led(void)
+{
+  GPIO_InitTypeDef GPIO_InitStructure;
+  DAC_InitTypeDef DAC_InitStructure;
+
+  /* DAC Periph clock enable */
+  RCC_APB1PeriphClockCmd(RCC_APB1Periph_DAC, ENABLE);
+
+  /* GPIOA clock enable */
+  RCC_AHBPeriphClockCmd(RCC_AHBPeriph_GPIOA, ENABLE);
+  /* Configure PA.04 (DAC_OUT1), PA.05 (DAC_OUT2) as analog */
+  GPIO_InitStructure.GPIO_Pin =  GPIO_Pin_4;
+  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AN;
+  GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL;
+  GPIO_Init(GPIOA, &GPIO_InitStructure);
+  
+  DAC_DeInit();
+  
+  DAC_InitStructure.DAC_Trigger = DAC_Trigger_None;
+  DAC_InitStructure.DAC_WaveGeneration = DAC_WaveGeneration_None;
+  DAC_InitStructure.DAC_OutputBuffer = DAC_OutputBuffer_Enable;
+  
+  /* DAC Channel2 Init */
+  DAC_Init(DAC_Channel_1, &DAC_InitStructure);
+
+  /* Enable DAC Channel2 */
+  DAC_Cmd(DAC_Channel_1, ENABLE);
+  
+  DAC_SetChannel1Data(DAC_Align_12b_R, 0x0); // 0x64D = 1.26V
+  
+  DAC_SetChannel1Data(DAC_Align_12b_R, 0x64D); // 0x64D = 1.26V
+//  DAC_DualSoftwareTriggerCmd(ENABLE);
+  
+  while (1) {
+    delay_ms(500);
+    DAC_SetChannel1Data(DAC_Align_12b_R, 0x0); // 0x64D = 1.26V
+    delay_ms(500);
+    DAC_SetChannel1Data(DAC_Align_12b_R, 0x64D); // 0x64D = 1.26V
+  }
+}
