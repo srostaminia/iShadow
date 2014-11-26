64ddde4198707decf87d02e7bec9b55ad4d627b7
AMMayberry1 <ammayberry1@gmail.com>
2014-11-26 15:45:31 -0500

    find_pupil_edge algorithm seems to be working, also slight bug fix to plot_vec_filtered

--- ann_model/find_pupil_edge.m
+++ ann_model/find_pupil_edge.m
@@ -1,4 +1,4 @@
-function [lower, upper] = find_pupil_edge(pixels)
+function [lower, upper] = find_pupil_edge(pixels, peak_thresh)
 
     % First do median filtering
     pixels = medfilt1(pixels,3);
@@ -11,13 +11,14 @@ function [lower, upper] = find_pupil_edge(pixels)
     edge_detect = abs(conv(pixels, conv_op, 'valid'));    
     
     % Then peak identification
-    peaks = find(edge_detect(2:end-1) > edge_detect(3:end) & edge_detect(2:end-1) > edge_detect(1:end-2) & edge_detect(2:end-1) >= 0.48);
+    peaks = find(edge_detect(2:end-1) > edge_detect(3:end) & edge_detect(2:end-1) > edge_detect(1:end-2) & edge_detect(2:end-1) >= peak_thresh);
+    peaks = peaks + 1;
     
     % Weed out peaks resulting from specular reflections
     i = 1;
     while i < length(peaks)
         if edge_detect(peaks(i)) > 1
-            tmp = find(edge_detect(1:peaks(i) < 1),1,'last');
+            tmp = find(edge_detect(1:peaks(i)) < 1,1,'last');
             
             if isempty(tmp)
                 peaks(i) = [];
@@ -26,22 +27,23 @@ function [lower, upper] = find_pupil_edge(pixels)
                 i = i+1; 
             end
             
-            if (i < length(peaks))
-                tmp = find(edge_detect(peaks(i+1):end) < 1,1);
+            if (i <= length(peaks))
+                tmp = find(edge_detect(peaks(i):end) < 1,1);
                 
                 if isempty(tmp)
-                    peaks(i+1:end) = [];
+                    peaks(i:end) = [];
                     break;
                 else
-                    peaks(i+1) = tmp + peaks(i+1) - 1;
+                    peaks(i) = tmp + peaks(i) - 1;
                 end
             end
             
         end
         i = i+1;
     end
-    
-    peaks = [1, peaks, length(edge_detect)];
+
+    peaks = peaks + conv_offset(1);
+    peaks = [1, peaks, length(pixels)];
     
     if (length(peaks) == 2)
         lower = conv_offset(1); upper = length(pixels) - conv_offset(2);
@@ -55,20 +57,29 @@ function [lower, upper] = find_pupil_edge(pixels)
     end
     
     % Identify range of lowest mean(s)
-    lower = 0; upper = 0;
+    rm_localmin = [];
+    rm_non_localmin = [];
     for i=1:length(region_means)
         if ( i == 1 || region_means(i) < region_means(i-1) ) && ( i == length(region_means) || region_means(i) < region_means(i+1) )
-            if (lower == 0)
-                lower = peaks(i);
-            end
-            
-            if peaks(i+1) > upper
-                upper = peaks(i + 1);
-            end
+            rm_localmin = [rm_localmin, i];
+        else
+            rm_non_localmin = [rm_non_localmin, i];
         end
     end
     
-    lower = lower + conv_offset(1);
-    upper = upper + conv_offset(1);
+    threshold = min(region_means(rm_non_localmin));
+    lower = 0; upper = 0;
+    
+    for i=1:length(rm_localmin)
+        if region_means(rm_localmin(i)) < threshold
+            if (lower == 0)
+                lower = peaks(rm_localmin(i));
+            end
+            
+            if peaks(rm_localmin(i) + 1) > upper
+                upper = peaks(rm_localmin(i) + 1);
+            end
+        end
+    end
 
 end
