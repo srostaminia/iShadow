019b77eb2797e27f512f7cae6a1940d79486cd4b
AMMayberry1 <ammayberry1@gmail.com>
2014-11-30 14:57:55 -0500

    Changed CIDER to use unnormalized pixel values

--- ann_model/plot_vec_filtered.m
+++ ann_model/plot_vec_filtered.m
@@ -13,10 +13,10 @@ function filtered=plot_vec_filtered(v, conv_op, title_str)
     title(title_str);
     
     conv_op_adj = conv_op / (sum(abs(conv_op(:))));
-    filtered = conv(v, conv_op_adj, 'valid');
+    filtered = abs(conv(v, conv_op_adj, 'valid'));
     
     coff = [floor(length(conv_op) / 2), ceil(length(conv_op) / 2)];
     subplot(3,1,2);
-    plot( (coff(1):length(v)-coff(2)) + 1, abs(filtered));
+    plot( (coff(1):length(v)-coff(2)) + 1, filtered);
 
 end
