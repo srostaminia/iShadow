fa742751fd5529e0870a1d86d977d776d992e55a
Yamin Tun <ytun@cs.umass.edu>
2015-02-03 09:47:14 -0500

    checkpoint- graph_error_model for radius_target and gaze

--- algorithms/cider/run_ann_sweep.m
+++ algorithms/cider/run_ann_sweep.m
@@ -4,37 +4,46 @@ function run_ann_sweep(result_dir, X, gout,nDim,scaleVect)
 %     addpath('../ann/lib');
 %     addpath('../ann/run_ann');
 
-    origin=pwd;
+origin=pwd;
+
+cd(result_dir);
+lambda_folders = cellstr(strsplit(ls));
+
+for i=1:length(lambda_folders)-1
+    cd(lambda_folders{i});
     
-    cd(result_dir);
-    lambda_folders = cellstr(strsplit(ls));
+    rep_files = cellstr(strsplit(ls('v7*')));
     
-    for i=1:length(lambda_folders)-1
-        cd(lambda_folders{i});
+    for j=1:length(rep_files)-1
+        fprintf('%s - %d\n',lambda_folders{i},j);
         
-        rep_files = cellstr(strsplit(ls('v7*')));
-        
-        for j=1:length(rep_files)-1
-            fprintf('%s - %d\n',lambda_folders{i},j);
-            
+        if nDim==3
+            [pred,radii]= ann(X, rep_files{j},nDim,scaleVect);
+        else
             [pred]= ann(X, rep_files{j},nDim,scaleVect);
-                      %  [pred,radii]= ann(X, rep_files{j},nDim,scaleVect);
-
-            filter = logical(sum(pred,2));
-            dist = sqrt(sum((gout(filter,:) - pred(filter,:)).^2,2));
-            err = mean(dist);
+        end
+        filter = logical(sum(pred,2));
+        dist = sqrt(sum((gout(filter,:) - pred(filter,:)).^2,2));
+        err = mean(dist);
+        
+        
+        if nDim==3
+            save('-V7',sprintf('ann_rep%d.mat',j),'pred','err','gout','avgRadEllipse','radii');
             
+        else
             save('-V7',sprintf('ann_rep%d.mat',j),'pred','err','gout');
-           %  save('-V7',sprintf('ann_rep%d.mat',j),'pred','err','gout','avgRadEllipse','radii');
-
-             %pred=predicted center, gout=ground truth center,
+            
         end
         
-        cd('..');
+        
+        %pred=predicted center, gout=ground truth center,
     end
     
-    cd(origin);
-    
-    fprintf('FINISHED');
-    
+    cd('..');
+end
+
+cd(origin);
+
+fprintf('FINISHED');
+
 end
--- algorithms/cider/run_cider_sweep.m
+++ algorithms/cider/run_cider_sweep.m
@@ -1,47 +1,52 @@
 function run_cider_sweep(result_dir, X, gout,nDim,scaleVect)
 %function run_cider_sweep(result_dir, X, gout,avgEllipseRad,nDim,scaleVect)
-    addpath('~/iShadow/algorithms/cider');
-    addpath('../ann/lib');
-    addpath('../ann/run_ann');
+addpath('~/iShadow/algorithms/cider');
+addpath('../ann/lib');
+addpath('../ann/run_ann');
 
-    origin = pwd;
-    
-    cd(result_dir);
-    lambda_folders = cellstr(strsplit(ls));
-    
-    for i=1:length(lambda_folders)-1
-        cd(lambda_folders{i});
-        
-        rep_files = cellstr(strsplit(ls('v7*')));
-        
-        for j=1:length(rep_files)-1
-            fprintf('%s - %d\n',lambda_folders{i},j);
-            
-            [chord_length,pred,radii,ann_used] = cider(X, rep_files{j}, 400, 0.22, 'circle_edge', 0,nDim,scaleVect);
-            
-            %Cider model
-            filter = logical(sum(pred,2));
-            dist = sqrt(sum((gout(filter,:) - pred(filter,:)).^2,2));
-            err = mean(dist);    
-            
-            %line model
-            filter_Line = logical(sum(pred,2)) & ~ann_used;
-            dist_Line = sqrt(sum((gout(filter_Line,:) - pred(filter_Line,:)).^2,2));
-            err_Line = mean(dist_Line); 
-            
-                        save('-V7',sprintf('cider_rep%d.mat',j),'chord_length','pred','radii','ann_used','dist','err','gout','err_Line');
+origin = pwd;
 
-          %  save('-V7',sprintf('cider_rep%d.mat',j),'chord_length','pred','radii','ann_used','dist','err','gout','err_Line','avgEllipseRad');
-            %pred=predicted center, gout=ground truth center,
-            %radii=estimated radii
-            %err=cider error
-            %err_Line=line model error
+cd(result_dir);
+lambda_folders = cellstr(strsplit(ls));
+
+for i=1:length(lambda_folders)-1
+    cd(lambda_folders{i});
+    
+    rep_files = cellstr(strsplit(ls('v7*')));
+    
+    for j=1:length(rep_files)-1
+        fprintf('%s - %d\n',lambda_folders{i},j);
+        
+        [chord_length,pred,radii,ann_used] = cider(X, rep_files{j}, 400, 0.22, 'circle_edge', 0,nDim,scaleVect);
+        
+        %Cider model
+        filter = logical(sum(pred,2));
+        dist = sqrt(sum((gout(filter,:) - pred(filter,:)).^2,2));
+        err = mean(dist);
+        
+        %line model
+        filter_Line = logical(sum(pred,2)) & ~ann_used;
+        dist_Line = sqrt(sum((gout(filter_Line,:) - pred(filter_Line,:)).^2,2));
+        err_Line = mean(dist_Line);
+        
+        
+        
+        if nDim==3
+            save('-V7',sprintf('cider_rep%d.mat',j),'chord_length','pred','radii','ann_used','dist','err','gout','err_Line');
+        else
+            
+            save('-V7',sprintf('cider_rep%d.mat',j),'chord_length','pred','radii','ann_used','dist','err','gout','err_Line','avgEllipseRad');
         end
-        
-        cd('..');
+        %pred=predicted center, gout=ground truth center,
+        %radii=estimated radii
+        %err=cider error
+        %err_Line=line model error
     end
     
-    cd(origin);
+    cd('..');
+end
 
-    fprintf('FINISHED');
+cd(origin);
+
+fprintf('FINISHED');
 end
