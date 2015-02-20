25c8e70813455d256a1c944878f19d3015a3b006
AMMayberry1 <ammayberry1@gmail.com>
2015-02-20 18:01:49 -0500

    Draft of ANN weight update code

---
+++ algorithms/ann/lib/update_mlp.m
@@ -0,0 +1,38 @@
+function [W,alpha] = train_mlp(Xtrain,ytrain,nHidden,lambda,W_groupSparse,alpha, maxiter)
+
+  nInstances = size(Xtrain,1);
+  nVars = size(Xtrain,2);
+  nTargets = size(ytrain,2);
+  nParams = nVars*nHidden + nHidden*nTargets;
+  
+  % Set up Objective Function
+  funObj = @(W)logisticmlp_obj(W,Xtrain,ytrain,nHidden);
+
+  % Set up Groups (don't penalize bias)
+  if(~isempty(nHidden))
+    groups = [repmat([1:nVars-1]',1,nHidden);zeros(1,nHidden)];
+    groups = [groups(:); zeros(nHidden*nTargets,1)];
+  else
+    groups = [repmat([1:nVars-1]',1,nTargets);zeros(1,nTargets)];
+    groups = groups(:);
+  end
+  nGroups = max(groups);
+
+  %Initialize auxiliary variables that will bound norm
+  penalizedFunObj = @(W)auxGroupLoss(W,groups,lambda,funObj);
+
+  % Set up L_1,2 Projection Function
+  [groupStart,groupPtr] = groupl1_makeGroupPointers(groups);
+  funProj = @(W)auxGroupL2Project(W,nParams,groupStart,groupPtr);
+
+  %Solve optimization problem
+  options.verbose=2;
+  options.maxIter = maxiter;
+
+  options.optTol = 1e-5; %default 1e-5
+  options.progTol  = 1e-9; %default 1e-9
+
+  Walpha = minConf_SPG(penalizedFunObj,[W_groupSparse(:);alpha],funProj,options);
+  W = Walpha(1:nParams);
+  W(abs(W) < 1e-5) = 0;
+  alpha = Walpha((1+nParams):end);
