c8d94516d528a7a00b12f1e26f344be67dfeeee9
AMMayberry1 <ammayberry1@gmail.com>
2015-02-25 16:16:12 -0500

    Updates to process_model to properly handle CIDER operation

--- utilities/rev3/process_model_binary.py
+++ utilities/rev3/process_model_binary.py
@@ -3,17 +3,22 @@ import sys
 import os
 import struct
 import pickle
+import numpy as np
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("model_folder", help="folder containing model data files")
     parser.add_argument("output_file", help="target file for array output")
     parser.add_argument("eye_fpn_mask", help="file containing FPN mask for eye-facing camera")
+    parser.add_argument("col_eye_fpn_mask", help="columnwise FPN mask for eye-facing camera (needed for CIDER)", nargs="?")
+    parser.add_argument("--columnwise", help="store ANN pixels in column-major order", action='store_true')
     args = parser.parse_args()
 
     folder = args.model_folder
     out_file = args.output_file
-    fpn_file = args.eye_fpn_mask
+    row_fpn_file = args.eye_fpn_mask
+    col_fpn_file = args.col_eye_fpn_mask
+    columnwise = args.columnwise
 
 #    if os.path.exists(out_file):
 #        print "ERROR: File", out_file, "already exists"
@@ -26,20 +31,31 @@ def main():
     wih = read_model_data(folder + "/nn_wih.txt", is_vector=False)
 
     try:
-        fpn_file = open(fpn_file, "r")
+        row_fpn_file = open(row_fpn_file, "r")
     except IOError:
-        print "ERROR: Could not open", fpn_file
+        print "ERROR: Could not open", row_fpn_file
         sys.exit()
 
-    fpn_data = pickle.load(fpn_file)
-    print len(fpn_data)
+    if col_fpn_file != None:
+        try:
+            col_fpn_file = open(col_fpn_file, "r")
+        except IOError:
+            print "ERROR: Could not open", col_fpn_file
+            sys.exit()
 
-    # sample_file = open("eye_test.pi")
-    # sample_data = pickle.load(sample_file)
-    # sample_file.close()
+    row_fpn_data = pickle.load(row_fpn_file)
+
+    if col_fpn_file != None:
+        col_fpn_data = pickle.load(col_fpn_file)
+        # col_fpn_data = col_fpn_data.T
 
     for i in range(len(mask)):
         mask[i][1] -= 1
+
+    if columnwise:
+        ind = np.lexsort((mask[:,0], mask[:,1]))
+        mask = np.array([[mask[i,0], mask[i,1]] for i in ind])
+        wih = np.array([wih[i,:] for i in ind])
 
     try:
         out_file = open(out_file + "_" + str(len(mask)) + "p" + ".raw", "wb")
@@ -47,11 +63,17 @@ def main():
         print "ERROR: Could not open", out_file
         sys.exit()
 
-    size = 32 * (len(bh) + len(bo) + (len(who) * len(who[0])) + (len(wih) * len(wih[0]))) + (16 * 2) * len(mask) * len(mask[0])
-    size = float(size) / 8192
+    model_size = 32 * (len(bh) + len(bo) + (len(who) * len(who[0])) + (len(wih) * len(wih[0]))) + 16 * len(mask) * len(mask[0])
+    model_size = float(model_size) / 8       # Convert from bits to bytes
+
+    fpn_size = 112 * 112 * 16
+    fpn_size *= 2 if col_fpn_file != None else 1
+    fpn_size = float(fpn_size) / 8          # Convert from bits to bytes
 
     print "Number of pixels:", len(mask)
-    print "Model memory footprint:", size, "KiB"
+    print "Model memory footprint:", '{0:.2f}'.format(model_size / 1000), "KB /", '{0:.2f}'.format(model_size / 1024), "KiB"
+    print "FPN mask(s) memory footprint:", '{0:.2f}'.format(fpn_size / 1000), "KB /", '{0:.2f}'.format(fpn_size / 1024), "KiB"
+    print "Total memory footprint:", '{0:.2f}'.format((model_size + fpn_size) / 1000), "KB /", '{0:.2f}'.format((model_size + fpn_size) / 1024), "KiB"
 
     # Store number of pixels
     out_file.write(struct.pack('H', len(mask)))
@@ -65,20 +87,14 @@ def main():
     write_model_data_binary(out_file, who, "who", "float", is_vector=False)
     write_model_data_binary(out_file, wih, "wih", "float", is_vector=False)
 
-    # print fpn_data[mask[0][0]][mask[0][1]]
-    # print fpn_data[mask[1][0]][mask[1][1]]
-    # print fpn_data[mask[2][0]][mask[2][1]]
-
-    # for i in range(len(mask)):
-        # out_file.write(struct.pack('H', fpn_data[mask[i][0]][mask[i][1]]))
-
     for i in range(112):
         for j in range(112):
-            out_file.write(struct.pack('H', fpn_data[i][j]))
+            out_file.write(struct.pack('H', row_fpn_data[i][j]))
 
-    # for i in range(112):
-    #     for j in range(112):
-    #         out_file.write(struct.pack('H', sample_data[i][j]))
+    if col_fpn_file != None:
+        for i in range(112):
+            for j in range(112):
+                out_file.write(struct.pack('H', col_fpn_data[i][j]))
 
     out_file.close()
 
@@ -104,7 +120,7 @@ def read_model_data(filename, is_vector, is_int=False):
                 data.append([float(datum) for datum in line.split()])
 
     data_file.close()
-    return data
+    return np.array(data)
 
 def write_model_data_binary(out_file, data, name, data_type, is_vector):
     if data_type == "float":
