cc260b9f1770c726ccd0228f625473ac322453d4
AMMayberry1 <ammayberry1@gmail.com>
2016-01-21 13:33:14 -0500

    unpack_images now dumps data to matlab file, with option to dump text and images (with eye track rendering\!\!)

--- utilities/rev3/unpack_images.py
+++ utilities/rev3/unpack_images.py
@@ -4,12 +4,44 @@ import sys
 import argparse
 import time
 import numpy as np
-import pylab
+import matplotlib.pyplot as plt
+from matplotlib.ticker import NullLocator
 import struct
 import pickle
 import os
 import shutil
+import scipy.io as sio
+from hdf5storage import savemat
 from utils import keyboard
+import itertools
+
+class SaveOptions:
+    def __init__(self, do_images, do_render, do_text):
+        self.do_images  = do_images
+        self.do_render  = do_render
+        self.do_text    = do_text
+        self.save_needed= do_images or do_text
+
+class ParamFields:
+    def __init__(self):
+        self.us_elapsed = float('NaN');
+        self.model_type = float('NaN');
+        self.pred_x = float('NaN');
+        self.pred_y = float('NaN');
+        self.cider_col = float('NaN');
+        self.cider_row = float('NaN');
+        self.cider_radius = float('NaN');
+
+    def __str__(self):
+        str_rep =  str(self.us_elapsed) + ", "
+        str_rep += str(self.model_type) + ", "
+        str_rep += str(self.pred_x) + ", "
+        str_rep += str(self.pred_y) + ", "
+        str_rep += str(self.cider_col) + ", "
+        str_rep += str(self.cider_row) + ", "
+        str_rep += str(self.cider_radius)
+
+        return str_rep
 
 def main():
     parser = argparse.ArgumentParser()
@@ -21,7 +53,10 @@ def main():
     parser.add_argument("--columnwise", action="store_true", help="images recorded columnwise on hardware instead of rowwise")
     parser.add_argument("--disknum", help="/dev/disk[N] to open (default = 2)", type=int)
     parser.add_argument("--overwrite", action="store_true", help="overwrite data folder if it already exists")
-    
+    parser.add_argument("--save-txt", action="store_true", help="save all the image data as separate .txt files")
+    parser.add_argument("--save-png", action="store_true", help="save all the images as separate .png image files")
+    parser.add_argument("--render", action="store_true", help="render eye tracking results in saved image files (ignored if images are not being saved)")
+
     groupA = parser.add_mutually_exclusive_group()
     groupA.add_argument("-o", "--offset", help="Number of images / pairs to skip on SD card", type=int)
     groupA.add_argument("--reuse-raw", action="store_true", help="Reuse previously stored raw image data files")
@@ -41,6 +76,8 @@ def main():
     overwrite = args.overwrite
     num_skip = args.offset if args.offset != None else 0
     outdoor_masks = args.outdoor_switch
+
+    save_options = SaveOptions(args.save_png, args.render, args.save_txt)
 
     unit_size = 25088
 
@@ -76,6 +113,10 @@ def main():
         outdoor_out_mask = None
         outdoor_eye_mask = None
 
+    if interleaved:
+        a_postfix = "_a.raw"
+    else:
+        a_postfix = ".raw"
 
     if args.reuse_raw:
         os.chdir(file_prefix)
@@ -115,9 +156,9 @@ def main():
         os.chdir(file_prefix)
 
         try:
-            output_a = open(file_prefix + "_a.raw", "wb")
+            output_a = open(file_prefix + a_postfix, "wb")
         except IOError:
-            print "Input file", file_prefix + "\\" + file_prefix + "_a.raw", "could not be opened."
+            print "Input file", file_prefix + "\\" + file_prefix + a_postfix, "could not be opened."
             sys.exit()
 
         if (interleaved):
@@ -127,6 +168,7 @@ def main():
                 print "Input file", file_prefix + "\\" + file_prefix + "_b.raw", "could not be opened."
                 sys.exit()
 
+        frame_results = []
         print "Reading image data..."
         if (num_images != None):
             for i in range(num_images):
@@ -149,7 +191,9 @@ def main():
                     output_b.write(data)
 
                 data = input_file.read(512)
-                parse_write_results(data, results_out)
+
+                # TODO: Make these variable names more sensible...
+                frame_results.append(parse_write_results(data, results_out))
 
             i += 1
 
@@ -183,12 +227,14 @@ def main():
                 if (interleaved):
                     output_b.write(data_b)
 
-                parse_write_results(data_param, results_out)
+                frame_results.append(parse_write_results(data_param, results_out))
 
                 i += 1
 
         num_images = i
         print "Total:", num_images, "images\n"
+
+        frame_results = np.array(frame_results)
 
         input_file.close()
         output_a.close()
@@ -197,9 +243,9 @@ def main():
             output_b.close()
 
     try:
-        output_a = open(file_prefix + "_a.raw", "rb")
+        output_a = open(file_prefix + a_postfix, "rb")
     except IOError:
-        print "Input file", file_prefix + "\\" + file_prefix + "_a.raw", "could not be opened."
+        print "Input file", file_prefix + "\\" + file_prefix + a_postfix, "could not be opened."
         sys.exit()
 
     if (interleaved):
@@ -209,11 +255,12 @@ def main():
             print "Input file", file_prefix + "\\" + file_prefix + "_b.raw", "could not be opened."
             sys.exit()
 
+    save_data = {"frame_results" : [str(param_fields) for param_fields in frame_results]}
     if (interleaved):
-        disp_save_images(output_a, eye_mask, file_prefix + "_eye", num_images, columnwise, outdoor_eye_mask)
-        disp_save_images(output_b, out_mask, file_prefix + "_out", num_images, columnwise, outdoor_out_mask)
+        save_data["images_eye"] = read_images(output_a, eye_mask, file_prefix + "_eye", num_images, columnwise, outdoor_eye_mask)
+        save_data["images_out"] = read_images(output_b, out_mask, file_prefix + "_out", num_images, columnwise, outdoor_out_mask)
     else:
-        disp_save_images(output_a, out_mask, file_prefix, num_images, columnwise)
+        save_data["images"] = read_images(output_a, out_mask, file_prefix, num_images, columnwise)
 
     output_a.close()
 
@@ -221,6 +268,8 @@ def main():
         output_b.close()
         
     results_out.close()
+
+    save_frames(file_prefix, save_data, save_options, frame_results)
 
     os.chdir("..")
 
@@ -254,14 +303,25 @@ def load_mask(mask_filename):
 
 
 def parse_write_results(result_data, out_file):
-    values1 = struct.unpack('I', result_data[0:4])
-    values2 = struct.unpack('b' * 6, result_data[4:10])
+    param_fields = ParamFields()
 
-    out_file.write(str(values1[0]) + ", ")
-    out_file.write(str(values2[0]))
+    param_fields.us_elapsed = struct.unpack('I', result_data[0:4])[0]
+    params = struct.unpack('b' * 6, result_data[4:10])
+
+    out_file.write(str(param_fields.us_elapsed) + ", ")
+    out_file.write(str(params[0]))
     for i in range(1,6):
-        out_file.write(", " + str(values2[i]))
+        out_file.write(", " + str(params[i]))
     out_file.write('\n')
+
+    param_fields.model_type = params[0]
+    param_fields.pred_x     = params[1]
+    param_fields.pred_y     = params[2]
+    param_fields.cider_col = params[3]
+    param_fields.cider_row = params[4]
+    param_fields.cider_radius = params[5]
+
+    return param_fields
 
 # Look for an all-zero param sector as EOF
 def is_eof(result_data):
@@ -274,10 +334,12 @@ def is_eof(result_data):
     return True
 
 
-def disp_save_images(image_file, mask_data, out_filename, num_images, columnwise, outdoor_mask=None):
+def read_images(image_file, mask_data, out_filename, num_images, columnwise, outdoor_mask=None):
     success = True
 
     print "Saving", out_filename + ":"
+
+    images = np.zeros((num_images, 112 * 111))
 
     i = 0
     while success and ((num_images == 0) or (i < num_images)):
@@ -288,36 +350,28 @@ def disp_save_images(image_file, mask_data, out_filename, num_images, columnwise
                 print i, '/', 
 
         if outdoor_mask == None:
-            success = read_packed_image(image_file, mask_data, out_filename, columnwise, i)
-        else:
-            success = read_packed_switching_image(image_file, mask_data, outdoor_mask, out_filename, columnwise, i)
+            images[i] = read_packed_image(image_file, mask_data, out_filename, columnwise, i)
+        # else:
+            # success = read_packed_switching_image(image_file, mask_data, outdoor_mask, out_filename, columnwise, i)
 
         i += 1
 
     print
 
+    return images
+
 def read_packed_image(image_file, mask_data, out_filename, columnwise, index):
-    image = []
-    for i in range(112):
-        image.append([])
-        
-        for j in range(112):
+    image = np.zeros((112,112))
+    for i, j in itertools.product(range(112), range(112)):
             data = image_file.read(2)
             
             if data == "":
-                # return []
-                return False
+                print "ERROR: Unexpected end of file found"
+                sys.exit()
             
             value = struct.unpack('h', data)[0]
             
-            image[i].append(value)
-
-    image = np.array(image)
-
-    # print image[0]
-    # image = image[1:]
-    
-    figure = pylab.figure()
+            image[i,j] = value
 
     if (columnwise):
         image = image.T
@@ -325,22 +379,8 @@ def read_packed_image(image_file, mask_data, out_filename, columnwise, index):
     image -= mask_data
     image = image[1:]
 
-    pylab.figimage(image, cmap = pylab.cm.Greys_r)
-
-    figure.set_size_inches(1, 1)
-
-    pylab.savefig(out_filename + "_" + ("%06d" % index) + ".png", dpi=112)
-
-    pylab.close()
-
-    text_file = open(out_filename + "_" + ("%06d" % index) + ".txt", 'w')
-    for line in image:
-        for item in line:
-            text_file.write(str(item) + ' ')
-        text_file.write('\n')
-    text_file.close()
-
-    return True
+    # return flattened array
+    return image.ravel()
 
 def read_packed_switching_image(image_file, mask_data, outdoor_mask, out_filename, columnwise, index):
     image = []
@@ -358,7 +398,7 @@ def read_packed_switching_image(image_file, mask_data, outdoor_mask, out_filenam
             
             image[i].append(value)
     
-    figure = pylab.figure()
+    figure = plt.figure()
 
     image = np.array(image)
 
@@ -376,13 +416,13 @@ def read_packed_switching_image(image_file, mask_data, outdoor_mask, out_filenam
     if (columnwise):
         image = image.T
 
-    pylab.figimage(image, cmap = pylab.cm.Greys_r)
+    plt.figimage(image, cmap = plt.cm.Greys_r)
 
     figure.set_size_inches(1, 1)
 
-    pylab.savefig(out_filename + "_" + ("%06d" % index) + "_" + switch_text + ".png", dpi=112)
+    plt.savefig(out_filename + "_" + ("%06d" % index) + "_" + switch_text + ".png", dpi=112)
 
-    pylab.close()
+    plt.close()
 
     text_file = open(out_filename + "_" + ("%06d" % index) + "_" + switch_text + ".txt", 'w')
     for line in image:
@@ -392,5 +432,56 @@ def read_packed_switching_image(image_file, mask_data, outdoor_mask, out_filenam
     text_file.close()
 
     return True
+
+def save_frames(filename, data, save_options, all_param_fields):
+    savemat(filename + ".mat", data, format='7.3', store_python_metadata=True)
+
+    if not save_options.save_needed:
+        return
+
+    for data_pair in data.items():
+        if data_pair[0] == "frame_results":
+            continue
+
+        images = data_pair[1]
+        for (i, image_orig), param_fields in itertools.izip(enumerate(images), all_param_fields):
+            image = image_orig.reshape((111,112))
+
+            if save_options.do_images:
+                fig = plt.figure() 
+                im_display=plt.imshow(image, cmap = plt.cm.Greys_r, interpolation='nearest')
+                # plt.figimage(image, cmap = plt.cm.Greys_r)
+
+                ax=fig.add_subplot(111)
+                ax.set_xlim([0, 112])
+                ax.set_ylim([112, 0])
+                
+                if save_options.do_render:
+                    if param_fields.model_type != 0:
+                        ax.plot([param_fields.pred_x, param_fields.pred_x], [max(0, param_fields.pred_y - 10), min(111, param_fields.pred_y + 10)], 'r-', linewidth=1)
+                        ax.plot([max(0, param_fields.pred_x - 10), min(111, param_fields.pred_x + 10)], [param_fields.pred_y, param_fields.pred_y], 'r-', linewidth=1)
+
+                        if param_fields.model_type == 1 or param_fields.model_type == 2:
+                            ax.plot([param_fields.cider_col, param_fields.cider_col], [0, 112], 'b-', linewidth=1)
+                            ax.plot([0, 112], [param_fields.cider_row, param_fields.cider_row], 'b-', linewidth=1)
+                            circle = plt.Circle((param_fields.pred_x, param_fields.pred_y), param_fields.cider_radius, fill=False, linewidth=1, color='g')
+                            ax.add_artist(circle)
+
+                fig.set_size_inches(1, 1)
+                frame1 = plt.gca()
+                frame1.axes.get_xaxis().set_visible(False)
+                frame1.axes.get_yaxis().set_visible(False)
+                plt.axis('off')
+                plt.savefig(filename + "_" + ("%06d" % i) + ".png", dpi=112,bbox_inches='tight',pad_inches=0)
+
+                plt.close()
+
+            if save_options.do_text:
+                text_file = open(filename + "_" + ("%06d" % i) + ".txt", 'w')
+                for line in image:
+                    for item in line:
+                        text_file.write(str(item) + ' ')
+                    text_file.write('\n')
+                text_file.close()
 
 main()
